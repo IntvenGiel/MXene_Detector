7349e62fb3f8a962a80eec22b0e7e1d8185b1294
IntvenGiel <giel.intven@gmail.com>
2025-01-24 11:14:38 +0100

    oops

--- main_file.py
+++ main_file.py
@@ -17,7 +17,7 @@ def create_optimized_systems(gas_name, mxene_name, functional, index):
     site = ineq_sites[index]
     unoptimized_systems = place_gas(mxene, gas_molecule, site, gas_cellsize, index, functional, current_dir)
     
-    for orientation in range(len(unoptimized_systems))
+    for orientation in range(len(unoptimized_systems)):
         system_optimize(gas_molecule, site, orientation, functional, current_dir)
 
 molecule_name = 'CO2'
